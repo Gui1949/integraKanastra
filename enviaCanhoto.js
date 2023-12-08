9a75303d2b24a5418c03075ae4fb51ebf034b9d8
Rafael Santana <rafael.almeida@medsystems.com.br>
2023-12-08 10:35:06 -0300

    Envio do canhoto (PUT)

--- enviaCanhoto.js
+++ enviaCanhoto.js
@@ -1,33 +1,143 @@
-const ftp = require("ftp");
+const fs = require("fs");
+const path = require("path");
+const Client = require("ssh2-sftp-client");
 
-const host = "speedtest.tele2.net";
-const username = "";
-const password = "";
-const filename = "5MB.zip";
-const localPath = "./downloads";
+async function downloadFiles() {
+  const sftp = new Client();
 
- const client = new ftp()
- client.connect(host, function(err) {
-    return null
-  if (err) {
-    console.log(err);
-    return;
-  }
+  try {
+    const config = {
+      host: "msp-thanos",
+      port: "22", // Usually 22 for SFTP
+      username: "desenvolvimento",
+      privateKey: fs.readFileSync("./chavem.pem"), // Use if connecting via private key
+    };
 
-  client.login(username, password, function(err) {
-    if (err) {
-      console.log(err);
-      return;
+    await sftp.connect(config);
+
+    const remoteDir = "/pdfs"; // Remote directory containing the files
+    const localDir = "./downloaded_files"; // Local directory where files will be downloaded
+
+    // Create the local directory if it doesn't exist
+    if (!fs.existsSync(localDir)) {
+      fs.mkdirSync(localDir);
     }
 
-    client.get(filename, localPath, function(err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
+    // List files in the remote directory
+    const files = await sftp.list(remoteDir);
 
-      console.log("Arquivo baixado com sucesso!");
-      client.end();
-    });
-  });
-});
+    // Download each file from the remote directory to the local directory
+    for (const file of files) {
+      const remoteFilePath = path.join(remoteDir, file.name);
+      const localFilePath = path.join(localDir, file.name);
+
+      await sftp.get(remoteFilePath, localFilePath);
+
+      const base64Pdf = fs.readFileSync(`./downloaded_files/${file.name}`, {
+        encoding: "base64",
+      });
+     
+      const login_snk = () => {
+        let credentials = {
+          username: "INTEGRA.EVUP",
+          password: "Med@sys22",
+        };
+
+        const base = "http://msp-ironman:8380";
+
+        // Define the URL for the login service
+        const url = base + "/mge/service.sbr?serviceName=MobileLoginSP.login";
+
+        // Construct the request body in the format required by the service
+        let content =
+          '<serviceRequest serviceName="MobileLoginSP.login"><requestBody><NOMUSU>' +
+          credentials.username +
+          "</NOMUSU><INTERNO>" +
+          credentials.password +
+          "</INTERNO></requestBody></serviceRequest>";
+
+        // Send a POST request to the service with the appropriate headers and request body
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "text/xml; charset=utf-8",
+            Accept: "*/*",
+            "Accept-Language": "en-GB",
+            "Accept-Encoding": "gzip, deflate",
+            Connection: "Keep-alive",
+            "Content-Length": content.length,
+          },
+          body: content,
+        })
+          .then((resp) => resp.text()) // Extract the response body as text
+          .then(function (data) {
+            // Log the response data for debugging purposes
+
+            // Find the start and end indices of the jsessionid element in the response body
+            let find_jsonid_ini = data.search("<jsessionid>");
+            let find_jsonid_fi = data.search("</jsessionid>");
+
+            // Extract the jsessionid value from the response body
+            jsonid = data.slice(find_jsonid_ini, find_jsonid_fi);
+            jsonid = jsonid.replace("<jsessionid>", "");
+
+            let criaRegistro = {
+              serviceName: "CRUDServiceProvider.saveRecord",
+              requestBody: {
+                dataSet: {
+                  rootEntity: "AD_CANHOTOFTP",
+                  includePresentationFields: "N",
+                  dataRow: {
+                    localFields: {
+                      CONTEUDO: {
+                        $: base64Pdf,
+                      },
+                      NUMNOTA: {
+                        $: file.name,
+                      },
+                    },
+                  },
+                  entity: {
+                    fieldset: {
+                      list: "ID",
+                    },
+                  },
+                },
+              },
+            };
+
+            fetch(
+              base +
+                "/mge/service.sbr?serviceName=CRUDServiceProvider.saveRecord&outputType=json",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "text/xml; charset=utf-8",
+                  Accept: "*/*",
+                  "Accept-Language": "en-GB",
+                  "Accept-Encoding": "gzip, deflate",
+                  Connection: "Keep-alive",
+                  Cookie: "JSESSIONID=" + jsonid,
+                },
+                body: JSON.stringify(criaRegistro),
+              }
+            )
+              .then((resp) => resp.text())
+              .then((resposta) => {
+                console.log(resposta);
+              });
+
+            // Send a JSON response to the client with the jsessionid value
+          });
+      };
+
+      login_snk();
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    sftp.end(); // Close the SFTP connection
+  }
+}
+
+downloadFiles();
--- sendCanhoto.js
+++ sendCanhoto.js
@@ -300,62 +300,81 @@ let integracao = (nureneg) => {
             .then((res) => res.json())
             .then((json) => {
               const body = {
-                externalId: linha[0][29],
-                sponsorName: linha[0][0],
-                sponsorGovernmentId: linha[0][1],
-                sponsorPersonType:
-                  linha[0][2] == "J" ? "LEGAL_PERSON" : "NATURAL_PERSON",
-                sponsorAddress: linha[0][3],
-                sponsorAddressNumber: linha[0][4],
-                sponsorNeighborhood: linha[0][5],
-                sponsorCity: linha[0][6],
-                sponsorState: linha[0][7],
-                sponsorCountry: linha[0][8],
-                sponsorZipCode: linha[0][9],
-                sellerName: linha[0][10],
-                sellerBank: 341,
-                sellerAgency: 149,
-                sellerAgencyDigit: 1,
-                sellerAccount: 4738,
-                sellerAccountDigit: 47381,
-                sellerGovernmentId: linha[0][11],
-                sellerPersonType: linha[0][12],
-                sellerAddress: linha[0][13],
-                sellerAddressNumber: linha[0][14],
-                sellerNeighborhood: linha[0][15],
-                sellerCity: linha[0][16],
-                sellerState: linha[0][17],
-                sellerCountry: linha[0][18],
-                sellerZipCode: linha[0][19],
-                coobrigation: false,
-                customFields: {},
-                items: [
+                "externalId": "myOwnId123",
+                "sponsorName": "Offer Sponsor",
+                "sponsorPersonType": "LEGAL_PERSON",
+                "sponsorGovernmentId": "19369866000100",
+                "sponsorExternalCode": "myOwnSponsorExternalCode",
+                "sponsorAddress": "ROD. SP 080 KM 69",
+                "sponsorAddressNumber": "N/A",
+                "sponsorAddressComplement": "Fazenda da roça",
+                "sponsorNeighborhood": "Zona Rural",
+                "sponsorCity": "São Paulo",
+                "sponsorState": "SP",
+                "sponsorCountry": "Brasil",
+                "sponsorZipCode": "26380-000",
+                "sponsorBank": "341",
+                "sponsorAgency": "1234",
+                "sponsorAgencyDigit": "0",
+                "sponsorAccount": "98765",
+                "sponsorAccountDigit": "0",
+                "sponsorPixKey": "12345678901",
+                "sellerName": "Offer Seller",
+                "sellerPersonType": "LEGAL_PERSON",
+                "sellerGovernmentId": "78659066000185",
+                "sellerExternalCode": "myOwnSellerExternalCode123",
+                "sellerAddress": "Av. Vinhedos",
+                "sellerAddressNumber": "71",
+                "sellerAddressComplement": "Torre Sul",
+                "sellerNeighborhood": "Jardim das Acacias",
+                "sellerCity": "Uberlandia",
+                "sellerState": "MG",
+                "sellerCountry": "Brasil",
+                "sellerZipCode": "26380-000",
+                "sellerBank": "341",
+                "sellerAgency": "1234",
+                "sellerAgencyDigit": "0",
+                "sellerAccount": "98765",
+                "sellerAccountDigit": "0",
+                "coobrigation": false,
+                "customFields": {},
+                "items": [
                   {
-                    assetType: "NOTA_COMERCIAL",
-                    invoiceNumber: linha[0][21].toString(),
-                    invoiceDate: format(dateFormated, "yyyyMMdd"),
-                    invoiceKey: linha[0][23],
-                    totalInstallments: linha[0][24],
-                    paymentValue: linha[0][28],
-                    // paymentDate: linha[0][26],
-                    paymentDate: "20240101",
-                    files: [
+                    "assetType": "DUPLICATA_MERCANTIL",
+                    "invoiceNumber": "000012",
+                    "invoiceDate": "20220101",
+                    "invoiceKey": "128391284917238917209381284190",
+                    "totalInstallments": 1,
+                    "paymentValue": 2000,
+                    "paymentDate": "20301231",
+                    "customFields": {},
+                    "files": [
                       {
-                        content: base64String,
-                        category: "nfe_pdf",
-                        name: "danfe.pdf",
-                      },
+                        "url": "https://www.kanastra.com.br/images/default/sample.pdf",
+                        "content": "",
+                        "category": "nfe_pdf",
+                        "name": "exampleCoverageFile.pdf"
+                      }
                     ],
-                    installments: itens,
-                  },
-                ],
-              };
+                    "installments": [
+                      {
+                        "externalId": "externalId_01",
+                        "amount": 2000,
+                        "dueDate": "20301231",
+                        "customFields": {}
+                      }
+                    ]
+                  }
+                ]
+              }
+
+              const offerId = "myOwnOfferId123";
 
               let url_ENVIO =
-                "https://hub-sandbox.kanastra.com.br/api/credit-originators/fidc-medsystem/offers";
+                `https://hub-sandbox.kanastra.com.br/api/credit-originators/fidc-medsystem/offers/${offerId}`;
 
               fetch(url_ENVIO, {
-                method: "POST",
+                method: "PUT",
                 headers: {
                   Accept: "application/json",
                   "Content-Type": "application/json",
@@ -459,9 +478,9 @@ let integracao = (nureneg) => {
 
 login_snk();
 
-app.get("/monitor/kanastra/envio", function (request, response) {
+app.get("/monitor/kanastra/envio_canhoto", function (request, response) {
   response.json({
-    envio: {
+    envio_canhoto: {
       total: total,
       incluidos: incluidos,
       atualizados: 0,
@@ -471,4 +490,4 @@ app.get("/monitor/kanastra/envio", function (request, response) {
   });
 });
 
-app.listen(process.env.PORT || 40001);
+app.listen(process.env.PORT || 4003);
