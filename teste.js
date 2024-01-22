259796e58fc55c2b4e78585500d4f4d571df391c
Gui1949 <guilhermesantiago12@gmail.com>
2024-01-22 10:06:07 -0300

    ✨ feat: envio de titulo via snk

--- teste.js
+++
@@ -1,5 +0,0 @@
-prolog = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>';
-var parser = new DOMParser();
-XmlStr = prolog + "<bookz/>";
-var xmlz  = parser.parseFromString(XmlStr, "application/xml");
-console.log(window.btoa((new XMLSerializer()).serializeToString(xmlz)));
