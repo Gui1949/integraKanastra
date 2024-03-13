fc2cfa42addd569fbe653b0217676b6b4fc671b3
Gui1949 <guilhermesantiago12@gmail.com>
2024-03-13 11:56:21 -0300

    🐞 fix: conversão com tratamento

--- enviaCanhoto.js
+++ enviaCanhoto.js
@@ -28,7 +28,7 @@ async function downloadFiles() {
       },
     });
 
-    await client.cd("/Life_Canhoto/Backup");
+    await client.cd("/Life_Canhoto");
 
     const files = await client.list();
 
@@ -62,7 +62,7 @@ async function downloadFiles() {
       );
       const pageToConvertAsImage = 1;
 
-      if (file.name != ".pdf") {
+      if (file.name.endsWith(".pdf")) {
         convert(pageToConvertAsImage, { responseType: "image" }).then(
           (resolve) => {
             console.log("Page 1 is now converted as image");
@@ -191,7 +191,133 @@ async function downloadFiles() {
 
             return resolve;
           }
+        ).catch((e) => {
+          return console.log('ERRO - ARQUIVO CORROMPIDO')
+        })
+      }
+      else{
+
+        const base64Pdf = fs.readFileSync(
+          `./downloaded_files/${file.name}`,
+          {
+            encoding: "base64",
+          }
         );
+
+        const login_snk = () => {
+          let credentials = {
+            username: "INTEGRA.EVUP",
+            password: "Med@sys22",
+          };
+
+          const base = "http://10.10.10.6:8180";
+
+          // Define the URL for the login service
+          const url =
+            base + "/mge/service.sbr?serviceName=MobileLoginSP.login";
+
+          // Construct the request body in the format required by the service
+          let content =
+            '<serviceRequest serviceName="MobileLoginSP.login"><requestBody><NOMUSU>' +
+            credentials.username +
+            "</NOMUSU><INTERNO>" +
+            credentials.password +
+            "</INTERNO></requestBody></serviceRequest>";
+
+          // Send a POST request to the service with the appropriate headers and request body
+          fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "text/xml; charset=utf-8",
+              Accept: "*/*",
+              "Accept-Language": "en-GB",
+              "Accept-Encoding": "gzip, deflate",
+              Connection: "Keep-alive",
+              "Content-Length": content.length,
+            },
+            body: content,
+          })
+            .then((resp) => resp.text()) // Extract the response body as text
+            .then(function (data) {
+              // Log the response data for debugging purposes
+
+              // Find the start and end indices of the jsessionid element in the response body
+              let find_jsonid_ini = data.search("<jsessionid>");
+              let find_jsonid_fi = data.search("</jsessionid>");
+
+              // Extract the jsessionid value from the response body
+              jsonid = data.slice(find_jsonid_ini, find_jsonid_fi);
+              jsonid = jsonid.replace("<jsessionid>", "");
+
+              let criaRegistro = {
+                serviceName: "CRUDServiceProvider.saveRecord",
+                requestBody: {
+                  dataSet: {
+                    rootEntity: "AD_CANHOTOFTP",
+                    includePresentationFields: "N",
+                    dataRow: {
+                      localFields: {
+                        CONTEUDO: {
+                          $: base64Pdf,
+                        },
+                        CHAVENFE: {
+                          $: file.name
+                            .replaceAll(".pdf", "")
+                            .replaceAll(" ", "")
+                            .replaceAll(".png", "")
+                            .replaceAll(".jpg", ""),
+                        },
+                      },
+                    },
+                    entity: {
+                      fieldset: {
+                        list: "ID",
+                      },
+                    },
+                  },
+                },
+              };
+
+              fetch(
+                base +
+                  "/mge/service.sbr?serviceName=CRUDServiceProvider.saveRecord&outputType=json",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "text/xml; charset=utf-8",
+                    Accept: "*/*",
+                    "Accept-Language": "en-GB",
+                    "Accept-Encoding": "gzip, deflate",
+                    Connection: "Keep-alive",
+                    Cookie: "JSESSIONID=" + jsonid,
+                  },
+                  body: JSON.stringify(criaRegistro),
+                }
+              )
+                .then((resp) => resp.text())
+                .then((resposta) => {
+                  console.log(resposta);
+
+                  if (resposta.tsError) {
+                    log_erros.push(
+                      "Nº Financeiro: " +
+                        +file.name.replace(".pdf", "") +
+                        " - " +
+                        resposta.statusMessage
+                    );
+                    erros++;
+                  }
+
+                  incluidos++;
+
+                  fs.unlinkSync(`./downloaded_files/${file.name}`);
+                });
+
+              // Send a JSON response to the client with the jsessionid value
+            });
+        };
+
+        login_snk();
       }
     }
   } catch (err) {
